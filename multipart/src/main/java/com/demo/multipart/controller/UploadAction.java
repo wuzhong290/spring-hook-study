c0671191cbf4d40f4ec031c9284dc4f84b1225c1
YOHO01\zhong.wu <zhong.wu>
2017-10-26 15:13:49 +0800

    upload

--- multipart/src/main/java/com/demo/multipart/controller/UploadAction.java
+++ multipart/src/main/java/com/demo/multipart/controller/UploadAction.java
@@ -1,25 +1,33 @@
 package com.demo.multipart.controller;
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.alibaba.fastjson.JSON;
 import com.demo.multipart.model.Plupload;
 import com.demo.multipart.utils.PluploadUtil;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by thinkpad on 2017/10/25.
  */
 @Controller
 @RequestMapping("/upload")
 public class UploadAction {
+    public static final String FileDir = "uploadfile/";
 
     @RequestMapping(value = "/upload")
     public String upload(@RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request, ModelMap model) {
@@ -44,8 +52,6 @@ public class UploadAction {
 
         return "result";
     }
-
-    public static final String FileDir = "uploadfile/";
 
     /**上传界面*/
     @RequestMapping("/uploadui")
@@ -82,5 +88,41 @@ public class UploadAction {
         }
 
         return "login.upload";
+    }
+
+    /**上传处理方法*/
+    @RequestMapping(value="/pluploadtest", method = RequestMethod.POST)
+    public void upload(HttpServletRequest request, HttpServletResponse response) {
+        if(StringUtils.equals(MediaType.APPLICATION_FORM_URLENCODED_VALUE, request.getContentType())){
+            Map<String, String[]> map =  request.getParameterMap();
+            try {
+                response.getWriter().write(JSON.toJSONString(map));
+                response.getWriter().flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if(StringUtils.contains( request.getContentType(), MediaType.MULTIPART_FORM_DATA_VALUE)){
+            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest)request;
+            MultiValueMap<String, MultipartFile> map = multipartRequest.getMultiFileMap();
+            for (Map.Entry<String, List<MultipartFile>> entry : map.entrySet()) {
+                System.out.println(entry.getKey());
+            }
+            Map<String, String[]> parameterMap =  request.getParameterMap();
+            try {
+                response.getWriter().write(JSON.toJSONString(parameterMap));
+                response.getWriter().flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if(StringUtils.contains( request.getContentType(), MediaType.APPLICATION_OCTET_STREAM_VALUE)){
+            //头部需要设置Content-Type = application/octet-stream
+            try {
+                List<String> files = IOUtils.readLines(request.getInputStream());
+                response.getWriter().write(JSON.toJSONString(files));
+                response.getWriter().flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
