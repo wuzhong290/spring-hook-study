883da4b7946d8bca807ee073c1e9bf0e2b40af2c
吴忠 <wuzhong@iqiguo.com>
2018-01-05 21:56:51 +0800

    cxf

--- cxf/src/main/java/com/demo/webservice/HelloServiceImpl.java
+++ cxf/src/main/java/com/demo/webservice/HelloServiceImpl.java
@@ -5,6 +5,7 @@ import javax.jws.WebService;
 @WebService(endpointInterface = "com.demo.webservice.HelloService")
 public class HelloServiceImpl implements HelloService {
 
+    @Override
     public String sayHello(String name) {
  		return "Hello " + name;
     }
---
+++ cxf/src/main/java/com/njqg/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.demo.com/")
+package com.njqg;
