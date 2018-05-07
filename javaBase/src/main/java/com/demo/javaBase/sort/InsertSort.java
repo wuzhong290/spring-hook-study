01e3c8033153cb18de76c1660eac77252a554b6f
吴忠 <wuzhong@iqiguo.com>
2018-05-07 17:57:07 +0800

    refactor

--- javaBase/src/main/java/com/demo/javaBase/sort/InsertSort.java
+++ javaBase/src/main/java/com/demo/javaBase/sort/InsertSort.java
@@ -5,14 +5,18 @@ import java.util.Arrays;
 public class InsertSort {
     public static void insertSort(int[] a) {
         int i, j, insertNote;// 要插入的数据
-        for (i = 1; i < a.length; i++) {// 从数组的第二个元素开始循环将数组中的元素插入
-            insertNote = a[i];// 设置数组中的第2个元素为第一次循环要插入的数据
+        //从数组的第二个元素开始循环将数组中的元素插入
+        for (i = 1; i < a.length; i++) {
+            // 设置数组中的第2个元素为第一次循环要插入的数据
+            insertNote = a[i];
             j = i - 1;
             while (j >= 0 && insertNote < a[j]) {
-                a[j + 1] = a[j];// 如果要插入的元素小于第j个元素,就将第j个元素向后移动
+                // 如果要插入的元素小于第j个元素,就将第j个元素向后移动
+                a[j + 1] = a[j];
                 j--;
             }
-            a[j + 1] = insertNote;// 直到要插入的元素不小于第j个元素,将insertNote插入到数组中
+            // 直到要插入的元素不小于第j个元素,将insertNote插入到数组中
+            a[j + 1] = insertNote;
         }
     }
 
