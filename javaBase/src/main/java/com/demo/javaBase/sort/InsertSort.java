1dfca7fe7921882614a9179ec8d8e203f4ba40fd
吴忠 <wuzhong@iqiguo.com>
2018-05-08 11:21:16 +0800

    插入排序

--- javaBase/src/main/java/com/demo/javaBase/sort/InsertSort.java
+++ javaBase/src/main/java/com/demo/javaBase/sort/InsertSort.java
@@ -3,7 +3,7 @@ package com.demo.javaBase.sort;
 import java.util.Arrays;
 
 public class InsertSort {
-    public static void insertSort(int[] a) {
+    public static void insertSortMinToMax(int[] a) {
         int i, j, insertNote;// 要插入的数据
         //从数组的第二个元素开始循环将数组中的元素插入
         for (i = 1; i < a.length; i++) {
@@ -20,9 +20,27 @@ public class InsertSort {
         }
     }
 
+    public static void insertSortMaxToMin(int[] a) {
+        int i, j, insertNote;// 要插入的数据
+        //从数组的第二个元素开始循环将数组中的元素插入
+        for (i = 1; i < a.length; i++) {
+            // 设置数组中的第2个元素为第一次循环要插入的数据
+            insertNote = a[i];
+            j = i - 1;
+            while (j >= 0 && insertNote > a[j]) {
+                // 如果要插入的元素大于第j个元素,就将第j个元素向后移动
+                a[j + 1] = a[j];
+                j--;
+            }
+            // 直到要插入的元素不小于第j个元素,将insertNote插入到数组中
+            a[j + 1] = insertNote;
+        }
+    }
+
     public static void main(String[] args) {
         int a[] = { 38,65,97,76,13,27,49 };
-        insertSort(a);
+        //insertSortMinToMax(a);
+        insertSortMaxToMin(a);
         System.out.println(Arrays.toString(a));
     }
 }
