8d4f812930ce0d1dc13d531ce8b3cd13e5ad2710
吴忠 <wuzhong@iqiguo.com>
2018-05-08 11:27:07 +0800

    init

--- javaBase/src/main/java/com/demo/javabase/sort/QuickSort.java
+++ javaBase/src/main/java/com/demo/javabase/sort/QuickSort.java
@@ -12,7 +12,8 @@ public class QuickSort {
 
     public static void quickSort(int[] a) {
         if(a.length>0) {
-            quickSortMinToMax(a, 0 , a.length-1);
+            //quickSortMinToMax(a, 0 , a.length-1);
+            quickSortMaxToMin(a, 0 , a.length-1);
         }
     }
 
@@ -51,5 +52,43 @@ public class QuickSort {
         quickSortMinToMax(a, low, i-1 );
         //6, 对key右边的数快排
         quickSortMinToMax(a, i+1, high);
+    }
+
+
+    private static void quickSortMaxToMin(int[] a, int low, int high) {
+        //1,找到递归算法的出口
+        if( low > high) {
+            return;
+        }
+        //2, 存
+        int i = low;
+        int j = high;
+        //3,key
+        int key = a[ low ];
+        //4，完成一趟排序
+        while( i< j) {
+            //4.1 ，从右往左找到第一个小于key的数
+            while(i<j && a[j] < key){
+                j--;
+            }
+            // 4.2 从左往右找到第一个大于key的数
+            while( i<j && a[i] >= key) {
+                i++;
+            }
+            //4.3 交换
+            if(i<j) {
+                int p = a[i];
+                a[i] = a[j];
+                a[j] = p;
+            }
+        }
+        // 4.4，调整key的位置
+        int p = a[i];
+        a[i] = a[low];
+        a[low] = p;
+        //5, 对key左边的数快排
+        quickSortMaxToMin(a, low, i-1 );
+        //6, 对key右边的数快排
+        quickSortMaxToMin(a, i+1, high);
     }
 }
