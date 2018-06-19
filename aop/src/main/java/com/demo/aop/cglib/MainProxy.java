2d78ec2ba6ef23e4c3c527cf3050b7563ba65379
吴忠 <wuzhong@iqiguo.com>
2018-06-19 14:35:56 +0800

    fix

--- aop/src/main/java/com/demo/aop/cglib/MainProxy.java
+++ aop/src/main/java/com/demo/aop/cglib/MainProxy.java
@@ -10,15 +10,11 @@ public class MainProxy {
         ProxyFactory proxyFactory = new ProxyFactory(CarNoInterface.class);
         proxyFactory.setProxyTargetClass(true);
         proxyFactory.setTarget(new CarNoInterface());
-        proxyFactory.addAdvice(new MethodInterceptor(){
-
-            @Override
-            public Object invoke(MethodInvocation invocation) throws Throwable {
-                System.out.println("事物开始");
-                Object proceed = invocation.proceed();
-                System.out.println("事物结束");
-                return  proceed;
-            }
+        proxyFactory.addAdvice((MethodInterceptor) (MethodInvocation invocation) -> {
+            System.out.println("事物开始");
+            Object proceed = invocation.proceed();
+            System.out.println("事物结束");
+            return  proceed;
         });
         CarNoInterface car = (CarNoInterface)proxyFactory.getProxy();
         car.run();
--- aop/src/main/java/com/demo/aop/jdk/MainProxy.java
+++ aop/src/main/java/com/demo/aop/jdk/MainProxy.java
@@ -1,14 +1,19 @@
 package com.demo.aop.jdk;
 
+import com.demo.aop.jdk.Car;
+import com.demo.aop.jdk.CarImpl;
 import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.framework.ProxyFactory;
 
 public class MainProxy {
 
     public static void main(String[] args) {
         //代理对象需要的实现的接口
-        final Class[] interfaces=new Class[]{Car.class};
+        Class[] interfaces=new Class[]{Car.class};
         ProxyFactory proxyFactory = new ProxyFactory(interfaces);
+        //决定采取哪种代理方式，如果设为true,代理方式就是CGlib
+        //proxyFactory.setProxyTargetClass(true);
         proxyFactory.setTarget(new CarImpl());
         proxyFactory.addAdvice((MethodInterceptor) invocation -> {
             System.out.println("事物开始");
