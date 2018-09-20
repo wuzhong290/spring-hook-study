57dcda4a6d8fe9bbcb8de587cce734203267e26e
吴忠 <wuzhong@iqiguo.com>
2018-09-20 18:42:48 +0800

    fix

--- doc/hello.lua
+++ doc/hello.lua
@@ -1,2 +1,4 @@
+--ngx.header.content_type = "application/json"
 local request_method = ngx.var.request_method
 ngx.say('hello ngx_lua!!!!',request_method);
+ngx.say('cookie_JSESSIONID',ngx.var.cookie_JSESSIONID);
