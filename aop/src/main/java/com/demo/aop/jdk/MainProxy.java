4f0db345640603208ca14eb4be84c0e35704595c
吴忠 <wuzhong@iqiguo.com>
2018-11-02 17:21:53 +0800

    fix

--- aop/src/main/java/com/demo/aop/jdk/MainProxy.java
+++ aop/src/main/java/com/demo/aop/jdk/MainProxy.java
@@ -1,16 +1,13 @@
 package com.demo.aop.jdk;
 
-import com.demo.aop.jdk.Car;
-import com.demo.aop.jdk.CarImpl;
 import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.framework.ProxyFactory;
 
 public class MainProxy {
 
     public static void main(String[] args) {
         //代理对象需要的实现的接口
-        Class[] interfaces=new Class[]{Car.class};
+        Class[] interfaces = CarImpl.class.getInterfaces();
         ProxyFactory proxyFactory = new ProxyFactory(interfaces);
         //决定采取哪种代理方式，如果设为true,代理方式就是CGlib
         //proxyFactory.setProxyTargetClass(true);
