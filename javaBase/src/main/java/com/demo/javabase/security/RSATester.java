3f61f4cb2b7e7ecd3ba1dbf4f5cfefccd64e0435
吴忠 <wuzhong@iqiguo.com>
2018-11-17 23:22:16 +0800

    fix

---
+++ javaBase/src/main/java/com/demo/javabase/security/RSATester.java
@@ -0,0 +1,66 @@
+package com.demo.javabase.security;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.util.Map;
+
+public class RSATester {
+
+    static String publicKey;
+    static String privateKey;
+
+    static {
+        try {
+            Map<String, Object> keyMap = RSAUtils.genKeyPair();
+            publicKey = RSAUtils.getPublicKey(keyMap);
+            privateKey = RSAUtils.getPrivateKey(keyMap);
+            System.err.println("公钥: \n\r" + publicKey);
+            System.err.println("私钥： \n\r" + privateKey);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //test();
+        //testSign();
+        test1();
+    }
+
+    static void test1() throws Exception{
+        byte [] encrypted = RSAUtils.encryptByPrivateKey("This is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret message".getBytes("utf-8"), privateKey);
+        System.out.println(Base64.encodeBase64String(encrypted));
+        // decrypt the message
+        byte[] secret = RSAUtils.decryptByPublicKey(encrypted, publicKey);
+        System.out.println(new String(secret, "utf-8"));
+    }
+
+    static void test() throws Exception {
+        System.err.println("公钥加密——私钥解密");
+        String source = "这是一行没有任何意义的文字，你看完了等于没看，不是吗？";
+        System.out.println("\r加密前文字：\r\n" + source);
+        byte[] data = source.getBytes();
+        byte[] encodedData = RSAUtils.encryptByPublicKey(data, publicKey);
+        System.out.println("加密后文字：\r\n" + new String(encodedData));
+        byte[] decodedData = RSAUtils.decryptByPrivateKey(encodedData, privateKey);
+        String target = new String(decodedData);
+        System.out.println("解密后文字: \r\n" + target);
+    }
+
+    static void testSign() throws Exception {
+        System.err.println("私钥加密——公钥解密");
+        String source = "这是一行测试RSA数字签名的无意义文字";
+        System.out.println("原文字：\r\n" + source);
+        byte[] data = source.getBytes();
+        byte[] encodedData = RSAUtils.encryptByPrivateKey(data, privateKey);
+        System.out.println("加密后：\r\n" + new String(encodedData));
+        byte[] decodedData = RSAUtils.decryptByPublicKey(encodedData, publicKey);
+        String target = new String(decodedData);
+        System.out.println("解密后: \r\n" + target);
+        System.err.println("私钥签名——公钥验证签名");
+        String sign = RSAUtils.sign(encodedData, privateKey);
+        System.err.println("签名:\r" + sign);
+        boolean status = RSAUtils.verify(encodedData, publicKey, sign);
+        System.err.println("验证结果:\r" + status);
+    }
+}
--- javaBase/src/main/java/com/demo/javabase/security/RsaSample.java
+++ javaBase/src/main/java/com/demo/javabase/security/RsaSample.java
@@ -1,12 +1,11 @@
 package com.demo.javabase.security;
 
+import org.apache.commons.codec.binary.Base64;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.security.*;
 import java.security.interfaces.RSAPrivateKey;
@@ -18,6 +17,7 @@ import java.security.spec.X509EncodedKeySpec;
 public class RsaSample {
     public static final String RSA_ALGORITHM = "RSA";
     public static final Charset UTF8 = Charset.forName("UTF-8");
+    public static final String SIGN_ALGORITHMS = "SHA1WithRSA";
 
     public static void main(String [] args) throws Exception {
         // generate public and private keys
@@ -26,7 +26,7 @@ public class RsaSample {
         PrivateKey privateKey = keyPair.getPrivate();
 
         // encrypt the message
-        byte [] encrypted = encrypt(privateKey, "This is a secret message");
+        byte [] encrypted = encrypt(privateKey, "This is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret messageThis is a secret message");
         System.out.println(base64Encode(encrypted));
 
         // decrypt the message
@@ -51,8 +51,27 @@ public class RsaSample {
     public static byte[] decrypt(PublicKey publicKey, byte [] encrypted) throws Exception {
         Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, publicKey);
+        InputStream ins = new ByteArrayInputStream(encrypted);
+        ByteArrayOutputStream writer = new ByteArrayOutputStream();
+        //rsa解密的字节大小最多是128，将需要解密的内容，按128位拆开解密
+        byte[] buf = new byte[128];
+        int bufl;
 
-        return cipher.doFinal(encrypted);
+        while ((bufl = ins.read(buf)) != -1) {
+            byte[] block = null;
+
+            if (buf.length == bufl) {
+                block = buf;
+            } else {
+                block = new byte[bufl];
+                for (int i = 0; i < bufl; i++) {
+                    block[i] = buf[i];
+                }
+            }
+
+            writer.write(cipher.doFinal(block));
+        }
+        return writer.toByteArray();
     }
 
     /**
@@ -110,5 +129,67 @@ public class RsaSample {
     }
     public static byte[] base64Decode(String data) throws IOException {
         return new BASE64Decoder().decodeBuffer(data);
+    }
+
+    /**
+     * RSA签名
+     *
+     * @param content       待签名数据
+     * @param privateKey    商户私钥
+     * @param input_charset 编码格式
+     * @return 签名值
+     */
+    public static String sign(String content, String privateKey, String input_charset) {
+        try {
+            PKCS8EncodedKeySpec priPKCS8 = new PKCS8EncodedKeySpec(Base64.decodeBase64(privateKey));
+            KeyFactory keyf = KeyFactory.getInstance("RSA");
+            PrivateKey priKey = keyf.generatePrivate(priPKCS8);
+
+            java.security.Signature signature = java.security.Signature
+                    .getInstance(SIGN_ALGORITHMS);
+
+            signature.initSign(priKey);
+            signature.update(content.getBytes(input_charset));
+
+            byte[] signed = signature.sign();
+
+            return Base64.encodeBase64String(signed);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * RSA验签名检查
+     *
+     * @param content        待签名数据
+     * @param sign           签名值
+     * @param ali_public_key 支付宝公钥
+     * @param input_charset  编码格式
+     * @return 布尔值
+     */
+    public static boolean verify(String content, String sign, String ali_public_key, String input_charset) {
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            byte[] encodedKey = Base64.decodeBase64(ali_public_key);
+            PublicKey pubKey = keyFactory.generatePublic(new X509EncodedKeySpec(encodedKey));
+
+
+            java.security.Signature signature = java.security.Signature
+                    .getInstance(SIGN_ALGORITHMS);
+
+            signature.initVerify(pubKey);
+            signature.update(content.getBytes(input_charset));
+
+            boolean bverify = signature.verify(Base64.decodeBase64(sign));
+            return bverify;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
